fcbbb5c02c22be5b906df882b32ef3fbc33ffc65
coin9marketcap-lab <coin9marketcap@gmail.com>
2025-11-17 17:05:35 +0100

    Create Footer.jsx

---
+++ app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 p-4 mt-auto text-center text-sm">
+      <p>© 2025 Dragons Trending. All rights reserved.</p>
+    </footer>
+  );
+}
